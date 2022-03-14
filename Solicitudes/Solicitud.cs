da3a5d8e45de719f8ae999cd32f5e895bb7b998c
Cristian Camilo Correa Grajales <ccamilo.correag@gmail.com>
2022-03-14 13:48:27 -0500

    Base

---
+++ Solicitudes/Solicitud.cs
@@ -0,0 +1,51 @@
+public record Solicitud
+{
+    public int Id { get; set; }
+    public string? NombreCompletoCliente { get; set; }
+    public int Plazo { get; set; }
+    public long Monto { get; set; }
+}
+
+public class SolicitudesDB
+{
+    private static List<Solicitud> _solicitudes = new List<Solicitud>()
+   {
+     new Solicitud{ Id=1, NombreCompletoCliente="Cristian Correa", Monto = 15000000, Plazo=15 },
+     new Solicitud{ Id=2, NombreCompletoCliente="Camilo Correa", Monto = 10000000, Plazo=10 },
+     new Solicitud{ Id=3, NombreCompletoCliente="Pedro Perez", Monto = 24000000, Plazo=24 }
+   };
+
+    public static List<Solicitud> GetSolicitudes()
+    {
+        return _solicitudes;
+    }
+
+    public static Solicitud? GetSolicitud(int id)
+    {
+        return _solicitudes.SingleOrDefault(pizza => pizza.Id == id);
+    }
+
+    public static Solicitud CreateSolicitud(Solicitud solicitud)
+    {
+        _solicitudes.Add(solicitud);
+        return solicitud;
+    }
+
+    public static Solicitud UpdateSolicitud(Solicitud update)
+    {
+        _solicitudes = _solicitudes.Select(solicitud =>
+        {
+            if (solicitud.Id == update.Id)
+            {
+                solicitud.NombreCompletoCliente = update.NombreCompletoCliente;
+            }
+            return solicitud;
+        }).ToList();
+        return update;
+    }
+
+    public static void RemovePizza(int id)
+    {
+        _solicitudes = _solicitudes.FindAll(solicitud => solicitud.Id == id).ToList();
+    }
+}
